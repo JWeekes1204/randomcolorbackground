ebb60f7c3365991bdb5cc9231dec548a2ae04dda
jweekes1204 <Jadenweekes1204@gmail.com>
2016-04-14 21:16:27 +0000

    Finally added the different color boxes

--- javascript/colors.js
+++ javascript/colors.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+    var colors = ["Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Gray", "Black"];
+    for (var i = 0; i < colors.length; i++) {
+        var myDiv = $("<div>");
+        myDiv.addClass("color-box");
+        myDiv.css("background-color", colors[i]);
+        $("body").append(myDiv);
+        
+    
+    }
+    
+});
 $("div").click(function(){
     $("div").append
-}
+});
